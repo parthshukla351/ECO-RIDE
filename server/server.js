71b73e87565ca1701384a8bda68f90887e41ab5e
Parth shukla <parthshukla351@gmail.com>
2026-08-29 23:01:11 +0530

    Updated project

--- server/server.js
+++ server/server.js
@@ -26,7 +26,7 @@ app.use(cookieParser());
 // =========================
 
 const allowedOrigins = [
-  process.env.CLIENT_URL,
+  '',
   'https://eco-ride-xi.vercel.app',
   'http://localhost:5173',
   'http://localhost:5174',
